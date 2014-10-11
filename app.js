d9835bff307729002b9c6d38dd2e15ea469d01a0
philippjauss <philipp@jauss.name>
2014-10-11 05:28:01 +0200

    Initial Upload
    Initialer upload damit die Quellen für diesen Prototyp auf github
    verfügbar sind

---
+++ app.js
@@ -0,0 +1,137 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * mobilegeeks: http://feeds.feedburner.com/netbooknewsde?format=xml
+ * caschys blog: http://feeds.feedburner.com/stadt-bremerhaven/dqXM?format=xml
+ * 
+ */
+
+var FeedSub = require('feedsub');
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+app.get('/', function(req,res){
+
+    res.sendFile(__dirname+'/index.html');
+});
+
+app.get('/css/normalize.css', function(req,res){
+    res.sendFile(__dirname+'/css/normalize.css');
+});
+
+
+app.get('/css/foundation.css', function(req,res){
+    res.sendFile(__dirname+'/css/foundation.css');
+});
+
+app.get('/css/app.css', function(req,res){
+    res.sendFile(__dirname+'/css/app.css');
+});
+
+app.get('/js/vendor/modernizr.js', function(req,res){
+    res.sendFile(__dirname+'/js/vendor/modernizr.js');
+});
+
+app.get('/js/vendor/jquery.js', function(req,res){
+    res.sendFile(__dirname+'/js/vendor/jquery.js');
+});
+
+app.get('/js/foundation.min.js', function(req,res){
+    res.sendFile(__dirname+'/js/foundation.min.js');
+});
+
+
+
+
+
+function queue(len) {
+    var ret = [];
+
+    ret.push = function() {
+        if(ret.length === len) ret.shift();
+        return Array.prototype.push.apply(this, arguments);
+    };
+
+    return ret;
+}
+
+var readers = [];
+var newsqueueLength = 100;
+var newsqueue = queue(newsqueueLength);
+var feedSources= [
+        "http://feeds.feedburner.com/stadt-bremerhaven/dqXM?format=xml",
+        "http://feeds.feedburner.com/netbooknewsde?format=xml",
+        "http://feeds.feedburner.com/mobiflip",
+        "http://www.heise.de/newsticker/heise.rdf",
+        "http://feeds.feedburner.com/mobilegeeksde?utm_source=feedburner&amp;utm_medium=feed&amp;utm_campaign=Feed%253A+netbooknewsde+%2528Netbooknews.de+-+das+Netbook+Blog%2529",
+        "http://www.tagesanzeiger.ch/digital/rss.html",
+        "http://rss.golem.de/rss.php?feed=RSS1.0",
+        "http://www.netbooknews.com/feed/",
+        "http://channel9.msdn.com/Feeds/RSS",
+        "http://feeds.feedburner.com/aktuell/feeds/rss",
+        "http://feeds.feedburner.com/ScottHanselman"
+        
+];
+
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+
+    for (var newsqueueIndex=0; newsqueueIndex < newsqueueLength; newsqueueIndex++){
+        socket.emit('newsfeed',newsqueue[newsqueueIndex]);
+    }
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
+
+
+http.listen(3000, function (){
+    console.log('listening on *:3000');
+});
+
+
+
+
+ 
+
+
+for (var i = 0; i < feedSources.length; i++) {
+    console.log(feedSources[i]);
+
+    readers.push(
+        new FeedSub(feedSources[i], {
+            interval: 1,
+            autoStart: true,
+            emitOnStart: true
+        })
+    );
+    readers[i].on('item', function(item){
+
+           var time = new Date();
+           var timestamp = time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds() +'---';
+           var msg= timestamp + '<a href="'+item.link+'" target="_blank">'+item.title+'</a>';
+           console.log(timestamp +"-- Titel: "+item.title);
+           console.log(timestamp +"-- Link: "+item.link);
+           console.log(timestamp +"-- pubdate: "+item.pubdate);
+           io.sockets.emit('newsfeed',msg);
+           newsqueue.push(msg);
+           
+           });
+    readers[i].on('error', function (){
+        console.log('unhandled error ');
+    });
+    console.log('readers pushed');
+    
+}
+
+console.log('Anzahl feeds: '+readers.length);
+
+
